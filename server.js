1de30c227c6024bb9efc645f3b500f59eb47c106
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2020-08-29 15:19:33 +0530

    Serverless code
    Signed-off-by: Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
-const express = require("express");
-const app = express()
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
 
-const port = 1337 || process.env.PORT;
+function updateDatabase(data) {
+    //   ... // update the database
+    return newValue;
+}
 
+app.use(express.static('public'));
+app.post('/updatestate', (req, res) => {
+    const newValue = updateDatabase(res.body);
+    res.json(newValue);
+});
 
-app.use(express.static('public'))
-
-app.post('/api/v1/newsletter',function(request,response){
-    response.status(400).json({
-        result:"POST API Working"
-    })
-})
-
-app.get('/api/v1/testing',function(request,response){
-
-    console.log("This GET API was Triggered");
-
-    response.status(400).json({
-        result:"APIs are Working correctly"
-    })
-})
-
-app.listen(port,()=>{
-    console.log(`Server Started at PORT = ${port}`)
-})
+module.exports.handler = serverless(app);
